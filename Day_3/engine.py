69f9ca7ecec55887e925b5fd0df787ac44d26242
Saransh Sood <saranshsood24@gmail.com>
2023-12-03 18:49:55 +0530

    Day 3: Part 1

--- Day_3/engine.py
+++ Day_3/engine.py
@@ -1,18 +1,20 @@
 from pprint import pprint
 import string
 
-
 SCHEMATIC = []
+SKELETON = []
 TOTAL = 0
 CHARACTERS = list(string.punctuation)
 
 
 def main():
-    with open("test_input.txt") as file:
+    with open("input.txt") as file:
         for line in file:
             render_map(line)
+            render_dummy_map(line)
     check_vicinity()
     #pprint(SCHEMATIC)
+    #print(SKELETON)
     print(TOTAL)
 
 def render_map(line):
@@ -29,41 +31,40 @@ def render_map(line):
     SCHEMATIC.append(row)
 
 
+def render_dummy_map(line):
+    return SKELETON.append([char for char in line])
+
+
 def check_vicinity():
     global TOTAL
     for rowno, row in enumerate(SCHEMATIC):
         charno = 0
         for colno, val in enumerate(row):
             charno += len(val)
-            print(charno)
             if val.isnumeric():
-                #print(val)
                 lower_row_neighbours = []
                 upper_row_neighbours = []
                 row_neighbours = [row[colno -1], row[colno +1]]
                 
-                if (rowno - 1) > 0:
-                    upper_row = SCHEMATIC[rowno - 1]
+                if (rowno - 1) >= 0:
+                    upper_row = SKELETON[rowno - 1]
                     for i in range(charno - len(val) - 1 , charno + 1):
                         if i < len(upper_row):
                             upper_row_neighbours.append(upper_row[i])
                 
-                if (rowno + 1) < len(SCHEMATIC):
-                    lower_row = SCHEMATIC[rowno + 1]
+                if (rowno + 1) < len(SKELETON):
+                    lower_row = SKELETON[rowno + 1]
                     for j in range(charno - len(val) - 1 , charno + 1):
                         if j < len(lower_row):
                             lower_row_neighbours.append(lower_row[j])
                 
-                neighbours = [lower_row_neighbours, row_neighbours, upper_row_neighbours]
-                #print(neighbours)
+                neighbours = [*lower_row_neighbours, *row_neighbours, *upper_row_neighbours]
                 for neighbour in neighbours:
                     if neighbour in CHARACTERS:
                         if neighbour != "." and neighbour != "\n":
                             TOTAL += int(val)
                             break
 
-
-                # print(val)
                 # for i in range(rowno - 1, rowno + 2):
                 #     for j in range(colno - 1, colno + len(val)):
                 #         if i >= 0 and j >= 0:
@@ -71,8 +72,6 @@ def check_vicinity():
                 #             if (SCHEMATIC[i][j]) in ['*', '#', '+', "$"]:
                 #                 # print(val)
                 #                 TOTAL += int(val)
-
-
 
 
 if __name__ == "__main__":

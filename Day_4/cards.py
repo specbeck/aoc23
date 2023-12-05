0589ef54d18f9b0b826199932b69f81de24aef30
Saransh Sood <saranshsood24@gmail.com>
2023-12-05 09:46:30 +0530

    Day 4 [On StandBy]

--- Day_4/cards.py
+++ Day_4/cards.py
@@ -1,9 +1,16 @@
 import re 
+SUM = 0
 
 def main():
     with open("test_input.txt") as file:
+        scratchcards = []
         for line in file:
-            parse_line(line)
+            win, draw = parse_line(line)
+            scratchcards.append(calculate_points(win, draw))
+    
+    print(f"The scratchcards are worth {sum(scratchcards)} points!")
+
+
 
 def parse_line(line):
     sections = line.split(":")
@@ -11,10 +18,46 @@ def parse_line(line):
     numbers = numbers.split("|")
     
     winning_nums = numbers[0]
+    lottery_nums = numbers[1]
     winning_nums = winning_nums.split(" ") 
-    
+    lottery_nums = lottery_nums.split(" ")
 
-    print(sections)
+    winning_nums = [num.strip(" \n") for num in winning_nums]
+    lottery_nums = [num.strip(" \n") for num in lottery_nums]
+
+    parsed_win = []
+    parsed_lot = []
+
+    for num in winning_nums:
+        if num == "":
+            pass
+        else:
+            parsed_win.append(int(num))
+    for num in lottery_nums:
+        if num == "":
+            pass
+        else:
+            parsed_lot.append(int(num))
+    #winning_nums = [int(num) for num in winning_nums]
+    #lottery_nums = [int(num) for num in lottery_nums]
+
+
+    return parsed_win, parsed_lot
+
+def calculate_points(winning, drawing):
+    points = 0
+    value = 1
+    consecutive_matches = 0
+    for i, num in enumerate(drawing):
+        if num in winning:
+            #print(num)
+            consecutive_matches += 1
+            for _ in range(1, consecutive_matches + 1):
+                value *= 2
+            points += value
+        consecutive_matches = 0
+    #print(points)
+    return points
 
 
 if __name__ == "__main__":
